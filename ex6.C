892d59c430cba9b3fb58ba16264e0e0f7686a6bb
lohustng219 <Long.BD2518221@sis.hust.edu.vn>
2026-05-25 17:20:36 +0700

    Fix binary conversion logic and output formatting

--- ex6.C
+++ ex6.C
@@ -7,22 +7,18 @@ int main()
 {
     int n;
     scanf("%d" , &n);
-    if (n <= 0 || n > 255) printf("%s" "Unsucessful!");
+    if (n <= 0 || n > 255) printf("%s" "Unsuccessful!");
     else
     {
-    memset(bi,0,sizeof(bi));
-    int i = 8;
-    while (n > 0)
-    {
-        bi[i] = n%2;
+       memset(bi,0,sizeof(bi));
+       int i = 7;
+       while (n > 0) {
+        bi[i] = n % 2;
         i--;
-        n = n/2;
-    }
-
-    for (int i = 1; i <= 8; i++)
-    {
+        n = n / 2;
+        }
+       for (int i = 0; i <= 7; i++) 
         printf("%d", bi[i]);
-    }
     }
 
     return 0;
